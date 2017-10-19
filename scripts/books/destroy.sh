f8380d1554d36e64f6664c56bc92e9306dc11c81
kmlm <kevin.mcaleese@libertymutual.com>
2017-10-19 11:33:56 -0400

    Working destroy curl script

---
+++ scripts/books/destroy.sh
@@ -0,0 +1,7 @@
+API="${API_ORIGIN:-http://localhost:4741}"
+URL_PATH="/books"
+curl "${API}${URL_PATH}/${ID}" \
+  --include \
+  --request DELETE \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=$TOKEN"
