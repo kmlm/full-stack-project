f009e54cdf837c1210c469a3ebc70b6262b44bb5
kmlm <kevin.mcaleese@libertymutual.com>
2017-10-19 10:55:53 -0400

    updated book route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :books
   resources :examples, except: %i[new edit]
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
   resources :users, only: %i[index show]
+  resources :books, except: [:new, :edit]
+
 end
