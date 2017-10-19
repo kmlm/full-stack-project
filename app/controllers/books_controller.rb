f17cf45dcc168e8db2688e0943c9d37d87c96c47
kmlm <kevin.mcaleese@libertymutual.com>
2017-10-19 12:32:44 -0400

    Adjustments to BookController / create script
    Adjusted methods and inherited Protected Controller
    Successful Curl Script for Create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,4 @@
-class BooksController < ApplicationController
+class BooksController < ProtectedController
   before_action :set_book, only: [:show, :update, :destroy]
 
   # GET /books
@@ -15,7 +15,7 @@ class BooksController < ApplicationController
 
   # POST /books
   def create
-    @book = Book.new(book_params)
+    @book = current_user.books.build(book_params)
 
     if @book.save
       render json: @book, status: :created, location: @book
@@ -36,12 +36,13 @@ class BooksController < ApplicationController
   # DELETE /books/1
   def destroy
     @book.destroy
+    head :no_content
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_book
-      @book = Book.find(params[:id])
+      @book = current_user.books.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
