b63486fb8e9d760fdb8c581dac3492e5027f2c19
kmlm <kevin.mcaleese@libertymutual.com>
2017-10-23 18:42:05 -0400

    Skip authentication before user index
    Displaying stats for how many wish lists there are and determined by
    number of users so skipped authentication before users and added route
    for index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ProtectedController
-  skip_before_action :authenticate, only: %i[signup signin]
+  skip_before_action :authenticate, only: %i[signup signin index]
 
   # POST '/sign-up'
   def signup
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
+  get 'index' => 'users#index'
   resources :users, only: %i[index show]
   resources :books, except: [:new, :edit]
 
